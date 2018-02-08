964b60013c4254a23426a9098cd60b6f3892ae4f
Yael Chavoya <realidadvirtualwm@gmail.com>
2018-02-07 23:30:21 -0600

    Archivos iniciales

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
 <html>
   <head>
     <meta charset="utf-8"/>
-    <title>Hola mundo</title>
-  </head>
+    <title>Pellier - Ropa Online</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/main.css" />
+    </head>
   <body>
-    <p>Holitas</p>
+    <?php include 'html/header.html' ?>
+    <?php include 'html/menu.html' ?>
+
+    Cuerpo
+
+    <?php include 'html/footer.html' ?>
   </body>
 </html>
