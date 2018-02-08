9b4b4ac03f0a5f9635074722fa4adcecdab45cd3
Yael Chavoya <realidadvirtualwm@gmail.com>
2018-02-08 00:39:32 -0600

    Footer, header y menu

--- index.php
+++ index.php
@@ -2,8 +2,12 @@
   <head>
     <meta charset="utf-8"/>
     <title>Pellier - Ropa Online</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+    integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="css/main.css" />
+    <link rel="stylesheet" type="text/css" href="css/header.css" />
+    <link rel="stylesheet" type="text/css" href="css/footer.css" />
+    <link rel="stylesheet" type="text/css" href="css/menu.css" />
     </head>
   <body>
     <?php include 'html/header.html' ?>
