52ecd52e253e72000a260a98b03510377d49da06
Yael Chavoya <realidadvirtualwm@gmail.com>
2018-02-20 18:40:14 -0600

    Creación página contacto

---
+++ contacto.php
@@ -0,0 +1,27 @@
+<html>
+  <head>
+    <meta charset="utf-8"/>
+    <title>Pellier - Ropa Online</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+    integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/main.css" />
+    <link rel="stylesheet" type="text/css" href="css/header.css" />
+    <link rel="stylesheet" type="text/css" href="css/footer.css" />
+    <link rel="stylesheet" type="text/css" href="css/menu.css" />
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+    integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+
+    </head>
+  <body>
+    <?php include 'html/header.html' ?>
+    <?php include 'html/menu.html' ?>
+
+    <?php include 'html/contactform.html' ?>
+
+
+    <?php include 'html/footer.html' ?>
+  </body>
+</html>
