53054a60bb22853ee5c9176289ad59f01d08f8c4
Yael Chavoya <realidadvirtualwm@gmail.com>
2018-02-25 20:31:59 -0600

    Base separación de productos

---
+++ php/productos-filter.php
@@ -0,0 +1,77 @@
+<?php
+
+if(isset($_GET["cat"])){
+  $cat = $_GET["cat"];
+} else {
+  $cat = 0;
+}
+
+if(isset($_GET["typ"])){
+  $typ = $_GET["typ"];
+} else {
+  $typ = 0;
+}
+
+switch($cat) {
+  case 1:
+    $cat_title = "Temporada";
+    break;
+  case 2:
+    $cat_title = " Chamarras";
+    break;
+  case 3:
+    $cat_title = "Camisas";
+    break;
+  case 4:
+    $cat_title = "Pantalones";
+    break;
+  case 5:
+    $cat_title = "Vestidos";
+    break;
+  case 6:
+    $cat_title = "Calzado";
+    break;
+}
+
+switch($cat){
+  case 1:
+    switch($typ){
+      case 0:
+        break;
+      case 1:
+        $cat_title .= " Primavera 2018"; break;
+      case 2:
+        $cat_title .= " Invierno 2017"; break;
+      case 3:
+        $cat_title .= " Otoño 2017"; break;
+      case 4:
+        $cat_title .= " Verano 2017"; break;
+    }
+    break;
+  case 5:
+    switch($typ){
+      case 0:
+        break;
+      case 1:
+        $cat_title .= " Cortos"; break;
+      case 2:
+        $cat_title .= " Largos"; break;
+    }
+    break;
+    default:
+    switch($typ){
+      case 0:
+        break;
+      case 1:
+        $cat_title .= " Hombre"; break;
+      case 2:
+        $cat_title .= " Mujer"; break;
+      case 3:
+        $cat_title .= " Niños"; break;
+      case 4:
+        $cat_title .= " Niñas"; break;
+      }
+}
+
+
+?>
---
+++ productos.php
@@ -0,0 +1,28 @@
+<html>
+  <head>
+    <meta charset="utf-8"/>
+    <title>Pellier - Ropa Online</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+    integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/main.css" />
+    <link rel="stylesheet" type="text/css" href="css/header.css" />
+    <link rel="stylesheet" type="text/css" href="css/footer.css" />
+    <link rel="stylesheet" type="text/css" href="css/menu.css" />
+    <link rel="stylesheet" type="text/css" href="css/product-list.css" />
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+    integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+
+    </head>
+  <body>
+    <?php include 'html/header.html' ?>
+    <?php include 'html/menu.html' ?>
+    <?php require 'php/productos-filter.php'?>
+
+    <h1 id="category-title"><?php echo $cat_title; ?></h1>
+
+    <?php include 'html/footer.html' ?>
+  </body>
+</html>
