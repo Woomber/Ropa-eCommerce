6ffa2382e14b11007110f982cf0ab383dae26b38
Yael Chavoya <realidadvirtualwm@gmail.com>
2018-03-17 16:03:12 -0600

    Fondo, favicon y centrar título

--- contacto.php
+++ contacto.php
@@ -2,6 +2,7 @@
   <head>
 	<meta charset="utf-8"/>
     <title>Pellier - Ropa Online</title>
+    <link rel="shortcut icon" type="image/png" href="favicon.png"/>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
     integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="css/main.css" />
--- php/contact-send.php
+++ php/contact-send.php
@@ -19,6 +19,7 @@ $fields = array('name' => 'Name', 'email' => 'Email', 'phone' => 'Phone', 'messa
 <head>
   <meta charset="utf-8" />
   <title>Contacto - Pellier</title>
+  <link rel="shortcut icon" type="image/png" href="../favicon.png"/>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
   integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
   <link rel="stylesheet" type="text/css" href="../css/main.css" />
